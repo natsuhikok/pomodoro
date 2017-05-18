d046e8ce230a39ef2457bd880f0fbbf98a9ec296
natsuhikok <kato@natsuhiko.jp>
2017-05-18 10:14:48 +0700

    initial commit based on https://github.com/electron/electron-quick-start

---
+++ app/renderer/index.js
@@ -0,0 +1,2 @@
+var text = document.createTextNode('Hello Electron!');
+document.getElementById('App').appendChild(text);
---
+++ readme.md
@@ -0,0 +1,6 @@
+## TODO
+* webpack
+* scss
+* pug
+* reload
+* copy package json from root directory
