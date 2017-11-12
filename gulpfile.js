d656c388b1efa6b32f1e8e746c75646007db251e
natsuhikok <kato@natsuhiko.jp>
2017-11-12 15:15:35 +1300

    Add: add gulp task to copy node_modules into dis directory

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,22 @@
 const gulp = require('gulp');
 const electron = require('electron-connect').server.create({ path: 'dist' });
 const fs = require('fs');
+const copydir = require('copy-dir');
 
 gulp.task('server', () => {
   electron.start();
   gulp.watch(['dist/main.js', 'dist/renderer/**/*'], electron.restart);
   // cant restart when it restart and reload in same time.
   // gulp.watch(['dist/renderer/**/*'], electron.reload);
+});
+gulp.task('copy:modules', () => {
+  return copydir('./node_modules', './dist/node_modules', (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('ok');
+    }
+  });
 });
 
 gulp.task('create:packageJson', () => {
